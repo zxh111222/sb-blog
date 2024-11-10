4e65024e95bcf5779c50032971663a5ffe2030d9
zxhhhxz1221 <1305894626@qq.com>
2024-11-10 14:15:37 +0800

    Init

---
+++ src/main/java/io/github/zxh111222/sbblog/SbBlogApplication.java
@@ -0,0 +1,13 @@
+package io.github.zxh111222.sbblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SbBlogApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/github/zxh111222/sbblog/SbBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.zxh111222.sbblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbBlogApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
