15e1e6e570ce3d9fb29e738d652b3b240a19a5c9
zxhhhxz1221 <1305894626@qq.com>
2024-11-10 15:26:14 +0800

    后台管理 - @PostMapping("blog/add")

--- src/main/java/io/github/zxh111222/sbblog/BackendBlogController.java
+++ src/main/java/io/github/zxh111222/sbblog/BackendBlogController.java
@@ -1,9 +1,7 @@
 package io.github.zxh111222.sbblog;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("backend")
@@ -22,5 +20,17 @@ public class BackendBlogController {
     public String show(@PathVariable String id) {
         System.out.println("id = " + id);
         return "backend/backend-show";
+    }
+
+    @GetMapping("blog/add")
+    public String add() {
+        return "backend/backend-add";
+    }
+
+    @PostMapping("blog/add")
+    @ResponseBody
+    public String save() {
+        System.out.println("接收到 Post 请求");
+        return "保存成功！";
     }
 }
