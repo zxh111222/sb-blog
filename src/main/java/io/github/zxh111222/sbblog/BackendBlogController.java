c89bfd51341cd4666539bb74216573901814fc8c
zxhhhxz1221 <1305894626@qq.com>
2024-11-10 15:06:04 +0800

    增加后台管理博客列表和详情页

--- src/main/java/io/github/zxh111222/sbblog/BackendBlogController.java
+++ src/main/java/io/github/zxh111222/sbblog/BackendBlogController.java
@@ -2,6 +2,7 @@ package io.github.zxh111222.sbblog;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -10,5 +11,16 @@ public class BackendBlogController {
     @GetMapping()
     public String index() {
         return "backend/index";
+    }
+
+    @GetMapping("blog")
+    public String list() {
+        return "backend/backend-list";
+    }
+
+    @GetMapping("blog/{id}")
+    public String show(@PathVariable String id) {
+        System.out.println("id = " + id);
+        return "backend/backend-show";
     }
 }
