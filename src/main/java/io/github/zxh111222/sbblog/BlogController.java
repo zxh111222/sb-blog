9134d104fda037b2efe442e1ed9947344e125fe7
zxhhhxz1221 <1305894626@qq.com>
2024-11-10 14:47:12 +0800

    @PathVariable 自动接受 url 路径变量 id

--- src/main/java/io/github/zxh111222/sbblog/BlogController.java
+++ src/main/java/io/github/zxh111222/sbblog/BlogController.java
@@ -2,6 +2,7 @@ package io.github.zxh111222.sbblog;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -11,6 +12,12 @@ public class BlogController {
     @GetMapping()
     public String list() {
         return "blog/blog-list";
+    }
+
+    @GetMapping("{id}")
+    public String show(@PathVariable String id) {
+        System.out.println("id = " + id);
+        return "blog/blog-show";
     }
 
 
