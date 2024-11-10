b744a232f5ee8d869f559fcef529d0754712da94
zxhhhxz1221 <1305894626@qq.com>
2024-11-10 15:45:18 +0800

    重构: 重新组织后台管理的路径和包名

--- src/main/java/io/github/zxh111222/sbblog/BackendBlogController.java
+++ src/main/java/io/github/zxh111222/sbblog/BackendBlogController.java
@@ -4,42 +4,37 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @Controller
-@RequestMapping("backend")
+@RequestMapping("backend/blog")
 public class BackendBlogController {
     @GetMapping()
-    public String index() {
-        return "backend/index";
-    }
-
-    @GetMapping("blog")
     public String list() {
         return "backend/list";
     }
 
-    @GetMapping("blog/{id}")
+    @GetMapping("{id}")
     public String show(@PathVariable String id) {
         System.out.println("id = " + id);
         return "backend/show";
     }
 
-    @GetMapping("blog/add")
+    @GetMapping("add")
     public String add() {
         return "backend/add";
     }
 
-    @PostMapping("blog/add")
+    @PostMapping("add")
     @ResponseBody
     public String save() {
         System.out.println("接收到 Post 请求");
         return "保存成功！";
     }
 
-    @GetMapping("blog/edit")
+    @GetMapping("edit")
     public String edit() {
         return "backend/edit";
     }
 
-    @PostMapping("blog/edit")
+    @PostMapping("edit")
     @ResponseBody
     public String update() {
         System.out.println("接收到 Post 请求");
---
+++ src/main/java/io/github/zxh111222/sbblog/backend/IndexController.java
@@ -0,0 +1,14 @@
+package io.github.zxh111222.sbblog.backend;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("backend")
+public class IndexController {
+    @GetMapping()
+    public String index() {
+        return "backend/index";
+    }
+}
