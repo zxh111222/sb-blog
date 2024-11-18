4f85d3783b110c4c3078a8df1c2d43c25dcb18f0
zxhhhxz1221 <1305894626@qq.com>
2024-11-18 16:39:18 +0800

    模拟获取 Blog数据 在页面中显示

--- src/main/java/io/github/zxh111222/sbblog/BlogController.java
+++ src/main/java/io/github/zxh111222/sbblog/BlogController.java
@@ -1,16 +1,26 @@
 package io.github.zxh111222.sbblog;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Controller
 @RequestMapping("blog")
 public class BlogController {
     @GetMapping()
-    public String list() {
+    public String list(Model model) {
+        List<Blog> blogs = new ArrayList<Blog>();
+        for (int i = 1; i <= 4; i++) {
+            Blog blog = new Blog(Long.parseLong(i + ""), "title-" + i, "content-blablabla" + i);
+            blogs.add(blog);
+        }
+        model.addAttribute("blogs", blogs);
         return "blog/blog-list";
     }
 
