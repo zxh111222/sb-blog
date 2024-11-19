087e3fad006e5139b75db7e863b18aba7ca3157f
zxhhhxz1221 <1305894626@qq.com>
2024-11-19 20:11:57 +0800

    增加 blog表 初始数据

---
+++ src/main/resources/static/sql/blog.sql
@@ -0,0 +1,4 @@
+INSERT INTO sb_blog.blog (id, content, cover, title) VALUES (1, 'maven 加速，修改镜像源maven 加速，修改镜像源maven 加速，修改镜像源', '/img/cover/maven-cover.png', 'maven 加速，修改镜像源');
+INSERT INTO sb_blog.blog (id, content, cover, title) VALUES (2, 'composer 加速，修改镜像源composer 加速，修改镜像源composer 加速，修改镜像源', '/img/cover/composer-cover.png', 'composer 加速，修改镜像源');
+INSERT INTO sb_blog.blog (id, content, cover, title) VALUES (3, 'yarn 加速，修改镜像源yarn 加速，修改镜像源yarn 加速，修改镜像源', '/img/cover/yarn-cover.png', 'yarn 加速，修改镜像源');
+INSERT INTO sb_blog.blog (id, content, cover, title) VALUES (4, 'npm 加速，修改镜像源npm 加速，修改镜像源npm 加速，修改镜像源', '/img/cover/npm-cover.png', 'npm 加速，修改镜像源');
