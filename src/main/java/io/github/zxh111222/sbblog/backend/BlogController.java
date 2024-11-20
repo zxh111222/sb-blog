8e174de57d107f6f9efeda94f8c0e3adf171a468
zxhhhxz1221 <1305894626@qq.com>
2024-11-20 12:17:41 +0800

    获取数据库中 Blog 数据, 在 backend/list 页面中显示

--- src/main/java/io/github/zxh111222/sbblog/backend/BlogController.java
+++ src/main/java/io/github/zxh111222/sbblog/backend/BlogController.java
@@ -4,7 +4,10 @@ import io.github.zxh111222.sbblog.Blog;
 import io.github.zxh111222.sbblog.BlogRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller("BackendBlogController")
 @RequestMapping("backend/blog")
@@ -14,7 +17,9 @@ public class BlogController {
 
 
     @GetMapping()
-    public String list() {
+    public String list(Model model) {
+        List<Blog> blogs = blogRepository.findAll();
+        model.addAttribute("blogs", blogs);
         return "backend/list";
     }
 
