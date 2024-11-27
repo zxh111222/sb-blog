97b5d7c1551f8504ce36a039e6cfdde59a6dd765
zxhhhxz1221 <1305894626@qq.com>
2024-11-27 19:47:04 +0800

    前台博客列表从数据库中读取，同时支持分页

--- src/main/java/io/github/zxh111222/sbblog/BlogController.java
+++ src/main/java/io/github/zxh111222/sbblog/BlogController.java
@@ -1,14 +1,20 @@
 package io.github.zxh111222.sbblog;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 
 @Controller
@@ -19,9 +25,18 @@ public class BlogController {
     BlogRepository blogRepository;
 
     @GetMapping()
-    public String list(Model model) {
-        List<Blog> blogs = blogRepository.findAll();
-        model.addAttribute("blogs", blogs);
+    public String list(Model model,
+                       @RequestParam(value = "page") Optional<Integer> page,
+                       @RequestParam(value = "size") Optional<Integer> size
+    ) {
+        int currentPage = page.orElse(1);
+        int pageSize = size.orElse(6);
+
+        Pageable pageable = PageRequest.of(currentPage - 1, pageSize, Sort.by("id").descending());
+
+        Page<Blog> pageContent = blogRepository.findAll(pageable);
+
+        model.addAttribute("page", pageContent);
         return "blog/blog-list";
     }
 
