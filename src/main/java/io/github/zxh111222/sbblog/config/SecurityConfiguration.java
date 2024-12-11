90058450bc77e3ab665abb2a7c28390b657d49d0
zxhhhxz1221 <1305894626@qq.com>
2024-12-11 20:21:34 +0800

    覆盖 spring-security 自带的登录页面 - 增加 SecurityFilterChain 配置

--- src/main/java/io/github/zxh111222/sbblog/config/SecurityConfiguration.java
+++ src/main/java/io/github/zxh111222/sbblog/config/SecurityConfiguration.java
@@ -2,15 +2,31 @@ package io.github.zxh111222.sbblog.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .formLogin(
+                        form -> form.loginPage("/login")
+                )
+                .authorizeHttpRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers( "/login", "/assets/**", "/build/**", "/img/**", "/vendor/**").permitAll()
+                        .anyRequest().authenticated()
+                )
+        ;
+        return http.build();
+    }
 
     @Bean
     public UserDetailsService userDetailsService() {
