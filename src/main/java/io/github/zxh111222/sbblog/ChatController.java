4cc424db6d0478377ba94b4ccb406c7cf9e8a6f6
zxhhhxz1221 <1305894626@qq.com>
2024-12-17 13:12:50 +0800

    尝试测试对话的功能...
    在网址中输入 `http://localhost:8080/ai/chat?input=您好` 可以得到回答

---
+++ src/main/java/io/github/zxh111222/sbblog/ChatController.java
@@ -0,0 +1,26 @@
+package io.github.zxh111222.sbblog;
+
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/ai")
+public class ChatController {
+
+    private final ChatClient chatClient;
+
+    public ChatController(ChatClient.Builder builder) {
+        this.chatClient = builder.build();
+    }
+
+    @GetMapping("/chat")
+    public String chat(String input) {
+        return this.chatClient.prompt()
+                .user(input)
+                .call()
+                .content();
+    }
+}
