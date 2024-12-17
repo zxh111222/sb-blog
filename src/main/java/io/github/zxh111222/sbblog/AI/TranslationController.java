a69b08cfc88bcbad58e34a01b6d6c3d6309d5de3
zxhhhxz1221 <1305894626@qq.com>
2024-12-17 16:36:20 +0800

    完成文本翻译，目前每个html标签都分为一块

--- src/main/java/io/github/zxh111222/sbblog/AI/TranslationController.java
+++ src/main/java/io/github/zxh111222/sbblog/AI/TranslationController.java
@@ -3,11 +3,19 @@ package io.github.zxh111222.sbblog.AI;
 
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.prompt.Prompt;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/ai")
@@ -20,9 +28,15 @@ public class TranslationController {
     }
 
     @GetMapping("/translate-chinese")
-    public Flux<String> translateChineseToEnglish(@RequestParam String input) {
-        Prompt prompt = new Prompt("将以下这段中文文本翻译为英文:" + input);
-        return this.chatClient.prompt(prompt)
-                .stream().content();
+    public Map<String, String> translateChineseToEnglish(@RequestParam String input) {
+        Prompt prompt = new Prompt("将以下这段中文文本翻译为英文(只管翻译，不需任何交流，不需要其他多余语言):" + input);
+
+
+        return Map.of(
+                "translatedText",
+                chatClient.prompt(prompt)
+                        .call().content()
+        );
     }
+
 }
