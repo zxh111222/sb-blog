9283c19fb385947709dadd06f4b26301792fbfd2
zxhhhxz1221 <1305894626@qq.com>
2025-01-12 18:47:41 +0800

    单元测试：测试用户具有特定的角色

--- src/test/java/io/github/zxh111222/sbblog/dao/UserRepositoryTest.java
+++ src/test/java/io/github/zxh111222/sbblog/dao/UserRepositoryTest.java
@@ -1,5 +1,6 @@
 package io.github.zxh111222.sbblog.dao;
 
+import io.github.zxh111222.sbblog.entity.Role;
 import io.github.zxh111222.sbblog.entity.User;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
@@ -8,6 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Optional;
+import java.util.Set;
 
 @SpringBootTest
 public class UserRepositoryTest {
@@ -29,6 +31,24 @@ public class UserRepositoryTest {
 
         Optional<User> optionalUser = userRepository.findFirstByName(userName);
         Assertions.assertTrue(optionalUser.isPresent());
+    }
+
+    @Test
+    void rbac(){
+        // admin 用户具有 admin 角色
+        // admin角色具有两个权限
+        String roleName = "admin";
+        Optional<User> ou = userRepository.findFirstByName(roleName);
+        Assertions.assertTrue(ou.isPresent());
+
+        User user = ou.get();
+        Set<Role> roles = user.getRoles();
+        int actual = roles.size();
+        Assertions.assertEquals(1, actual);
+
+        Role role = roles.iterator().next();
+        String actualName = role.getName();
+        Assertions.assertEquals(roleName, actualName);
     }
 
 }
