d17017af38854ac194918c53609359a172968900
zxhhhxz1221 <1305894626@qq.com>
2025-01-13 14:26:49 +0800

    Method Security
    @EnableMethodSecurity
    
    @PreAuthorize("isAuthenticated()")
    
    https://docs.spring.io/spring-security/reference/servlet/authorization/method-security.html

--- src/main/java/io/github/zxh111222/sbblog/config/SecurityConfiguration.java
+++ src/main/java/io/github/zxh111222/sbblog/config/SecurityConfiguration.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -15,6 +16,7 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfiguration {
 
     @Autowired
